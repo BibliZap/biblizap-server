fb90e04c6e30a9628ae34d2abec61de7700287ad
Raphaël Bentégeac <raphael@bentegeac.com>
2023-10-06 16:17:11 +0200

    fix: better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{post, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use thiserror::Error;
 
 #[derive(Debug, Deserialize)]
 struct RequestParameters {
@@ -8,14 +9,38 @@ struct RequestParameters {
     input_id_list: Vec<String>
 }
 
-#[post("/api")]
-async fn api(req_body: String) -> impl Responder {
-    let parameters = serde_json::from_str::<RequestParameters>(&req_body).unwrap();
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("snowball error")]
+    Biblizap(#[from] biblizap_rs::Error),
+    #[error("invalid parameters")]
+    JsonError(#[from] serde_json::Error),
+}
+
+async fn handle_request(req_body: &str) -> Result<Vec<biblizap_rs::Article>, Error> {
+    let parameters = serde_json::from_str::<RequestParameters>(req_body)?;
+
     let lens_api_key = "TdUUUOLUWn9HpA7zkZnu01NDYO1gVdVz71cDjFRQPeVDCrYGKWoY";
 
-    let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth, parameters.output_max_size, lens_api_key).await.unwrap();
-    let f = format!("{:?}", snowball);
-    HttpResponse::Ok().body(f)
+    let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth, parameters.output_max_size, lens_api_key).await?;
+
+    Ok(snowball)
+}
+
+#[post("/api")]
+async fn api(req_body: String) -> impl Responder {
+    let snowball = handle_request(&req_body).await;
+    
+    match snowball {
+        Ok(snowball) => {
+            let f = serde_json::to_string(&snowball).unwrap();
+            HttpResponse::Ok().body(f)
+        }
+        Err(error) => {
+            HttpResponse::InternalServerError().body(error.to_string())
+        }
+    }
+    
 }
 
 #[actix_web::main]
