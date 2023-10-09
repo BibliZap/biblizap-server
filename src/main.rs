08cdfa775f965d956341128577c5e6e7e070d0de
Raphaël Bentégeac <raphael@bentegeac.com>
2023-10-09 16:29:52 +0200

    fix: use new lib

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn handle_request(req_body: &str) -> Result<String, Error> {
 
     let lens_api_key = "TdUUUOLUWn9HpA7zkZnu01NDYO1gVdVz71cDjFRQPeVDCrYGKWoY";
 
-    let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth, parameters.output_max_size, lens_api_key).await?;
+    let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth, parameters.output_max_size, &biblizap_rs::SearchFor::Both, lens_api_key).await?;
 
     let json_str = serde_json::to_string(&snowball)?;
 
@@ -35,8 +35,7 @@ async fn api(req_body: String) -> impl Responder {
     
     match snowball {
         Ok(snowball) => {
-            let f = serde_json::to_string(&snowball).unwrap();
-            HttpResponse::Ok().body(f)
+            HttpResponse::Ok().body(snowball)
         }
         Err(error) => {
             HttpResponse::InternalServerError().body(error.to_string())
