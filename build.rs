c5abc3f61860869873c9d3a32cdb616aabb1a90b
Raphaël Bentégeac <raphael@bentegeac.com>
2024-02-11 09:13:37 +0100

    feat: pass lens api key by command line

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
+    change_detection::ChangeDetection::path("./biblizap-frontend/dist").generate();
     resource_dir("./biblizap-frontend/dist").build()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use biblizap_rs::SearchFor;
 use serde::Deserialize;
 use thiserror::Error;
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+struct AppConfig {
+    lens_api_key: String
+}
 
 #[derive(Debug, Deserialize)]
 struct SnowballParameters {
@@ -21,10 +25,8 @@ pub enum Error {
     JsonError(#[from] serde_json::Error),
 }
 
-async fn handle_request(req_body: &str) -> Result<String, Error> {
+async fn handle_request(req_body: &str, lens_api_key: &str) -> Result<String, Error> {
     let parameters = serde_json::from_str::<SnowballParameters>(req_body)?;
-
-    let lens_api_key = "TdUUUOLUWn9HpA7zkZnu01NDYO1gVdVz71cDjFRQPeVDCrYGKWoY";
 
     let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth.clamp(1, 3), parameters.output_max_size.clamp(1, 3000), &parameters.search_for, lens_api_key).await?;
 
@@ -33,9 +35,8 @@ async fn handle_request(req_body: &str) -> Result<String, Error> {
     Ok(json_str)
 }
 
-#[post("/api")]
-async fn api(req_body: String) -> impl Responder {
-    let snowball = handle_request(&req_body).await;
+async fn api(req_body: String, _: HttpRequest, config: web::Data<AppConfig>) -> impl Responder {
+    let snowball = handle_request(&req_body, &config.lens_api_key).await;
     
     match snowball {
         Ok(snowball) => {
@@ -49,14 +50,34 @@ async fn api(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let args = Args::parse();
+    let config = web::Data::new(AppConfig {lens_api_key: args.lens_api_key}); 
+
+    HttpServer::new(move || {
         let generated = generate();
 
         App::new()
-            .service(api)
+            .service(web::resource("/api")
+                .app_data(config.clone())
+                .route(web::post().to(api)))
             .service(actix_web_static_files::ResourceFiles::new("/", generated))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", args.port))?
     .run()
     .await
+}
+
+use clap::Parser;
+
+/// Run an instance of BibliZap
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Your Lens.org API key
+    #[arg(short, long)]
+    lens_api_key: String,
+
+    /// Port on which to listen
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16
 }
