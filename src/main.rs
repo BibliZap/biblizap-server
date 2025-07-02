3a65066f95f00395596e24466974157ac75d4a8b
Raphaël Bentégeac <raphael@bentegeac.com>
2025-07-02 10:33:22 +0200

    fix: formatting

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 /// Application configuration holding necessary secrets/settings.
 struct AppConfig {
-    lens_api_key: String
+    lens_api_key: String,
 }
 
 /// Parameters received from the frontend for the snowball search.
@@ -17,7 +17,7 @@ struct SnowballParameters {
     output_max_size: usize,
     depth: u8,
     input_id_list: Vec<String>,
-    search_for: SearchFor
+    search_for: SearchFor,
 }
 
 /// Custom error type for the server.
@@ -35,7 +35,14 @@ pub enum Error {
 async fn handle_request(req_body: &str, lens_api_key: &str) -> Result<String, Error> {
     let parameters = serde_json::from_str::<SnowballParameters>(req_body)?;
 
-    let snowball = biblizap_rs::snowball(&parameters.input_id_list, parameters.depth.clamp(1, 3), parameters.output_max_size.clamp(1, 3000), &parameters.search_for, lens_api_key).await?;
+    let snowball = biblizap_rs::snowball(
+        &parameters.input_id_list,
+        parameters.depth.clamp(1, 3),
+        parameters.output_max_size.clamp(1, 3000),
+        &parameters.search_for,
+        lens_api_key,
+    )
+    .await?;
 
     let json_str = serde_json::to_string(&snowball)?;
 
@@ -47,14 +54,10 @@ async fn handle_request(req_body: &str, lens_api_key: &str) -> Result<String, Er
 /// and returns the results as JSON or an error response.
 async fn api(req_body: String, _: HttpRequest, config: web::Data<AppConfig>) -> impl Responder {
     let snowball = handle_request(&req_body, &config.lens_api_key).await;
-    
+
     match snowball {
-        Ok(snowball) => {
-            HttpResponse::Ok().body(snowball)
-        }
-        Err(error) => {
-            HttpResponse::InternalServerError().body(format!("{:#?}", error))
-        }
+        Ok(snowball) => HttpResponse::Ok().body(snowball),
+        Err(error) => HttpResponse::InternalServerError().body(format!("{error:#?}")),
     }
 }
 
@@ -64,15 +67,19 @@ async fn api(req_body: String, _: HttpRequest, config: web::Data<AppConfig>) -> 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let config = web::Data::new(AppConfig {lens_api_key: args.lens_api_key}); 
+    let config = web::Data::new(AppConfig {
+        lens_api_key: args.lens_api_key,
+    });
 
     HttpServer::new(move || {
         let generated = generate();
 
         App::new()
-            .service(web::resource("/api")
-                .app_data(config.clone())
-                .route(web::post().to(api)))
+            .service(
+                web::resource("/api")
+                    .app_data(config.clone())
+                    .route(web::post().to(api)),
+            )
             .service(actix_web_static_files::ResourceFiles::new("/", generated))
     })
     .bind(("127.0.0.1", args.port))?
@@ -92,5 +99,5 @@ struct Args {
 
     /// Port on which to listen
     #[arg(short, long, default_value_t = 8080)]
-    port: u16
+    port: u16,
 }
