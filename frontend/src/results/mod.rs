3ae23ad557c6be88e818037d6868d677a555fc00
Raphaël Bentégeac <raphael@bentegeac.com>
2025-10-03 11:48:51 +0200

    refactor: move row into it's own module

--- frontend/src/results/mod.rs
+++ frontend/src/results/mod.rs
@@ -17,6 +17,9 @@ use footer::TableFooter;
 mod download;
 use download::*;
 
+mod row;
+use row::*;
+
 /// Enum representing the status of the search results.
 #[derive(Clone, PartialEq)]
 pub enum ResultsStatus {
@@ -381,59 +384,6 @@ fn table_global_filter(props: &TableGlobalSearchProps) -> Html {
                 <input type="text" class="form-control" oninput={oninput} ref={input_node_ref}/>
             </div>
         </div>
-    }
-}
-
-/// Properties for a table row component.
-#[derive(Clone, PartialEq, Properties)]
-pub struct RowProps {
-    article: Article,
-    update_selected: Callback<(String, bool)>,
-    selected_articles: HashSet<String>,
-}
-/// Component for a single row in the results table.
-/// Displays article information and a checkbox for selection.
-#[function_component(Row)]
-pub fn row(props: &RowProps) -> Html {
-    fn doi_link(doi: Option<String>) -> Option<String> {
-        let doi = doi?;
-        Some(format!("https://doi.org/{}", doi))
-    }
-
-    let is_selected = props
-        .article
-        .doi
-        .as_ref()
-        .map(|doi| props.selected_articles.contains(doi))
-        .unwrap_or(false);
-
-    let onchange = {
-        let update_selected = props.update_selected.clone();
-        let doi = props.article.doi.clone();
-        Callback::from(move |event: Event| {
-            let update_selected = update_selected.clone();
-            let doi = doi.clone();
-            let checked = event
-                .target_unchecked_into::<web_sys::HtmlInputElement>()
-                .checked();
-            if let Some(doi) = doi {
-                update_selected.emit((doi, checked))
-            }
-        })
-    };
-
-    html! {
-        <tr>
-            <td><input type={"checkbox"} class={"row-checkbox"} checked={is_selected} onchange={onchange}/></td>
-            <td style=""><a href={doi_link(props.article.doi.clone())} style="word-wrap: break-word">{props.article.doi.clone().unwrap_or_default()}</a></td>
-            <td style="word-wrap: break-word">{props.article.title.clone().unwrap_or_default()}</td>
-            <td style="word-wrap: break-word">{props.article.journal.clone().unwrap_or_default()}</td>
-            <td>{props.article.first_author.clone().unwrap_or_default()}</td>
-            <td>{props.article.year_published.unwrap_or_default()}</td>
-            <td>{props.article.summary.clone().unwrap_or_default()}</td>
-            <td>{props.article.citations.unwrap_or_default()}</td>
-            <td>{props.article.score.unwrap_or_default()}</td>
-        </tr>
     }
 }
 
---
+++ frontend/src/results/row.rs
@@ -0,0 +1,56 @@
+use super::Article;
+use std::collections::HashSet;
+use yew::prelude::*;
+
+/// Properties for a table row component.
+#[derive(Clone, PartialEq, Properties)]
+pub struct RowProps {
+    pub article: Article,
+    pub update_selected: Callback<(String, bool)>,
+    pub selected_articles: HashSet<String>,
+}
+/// Component for a single row in the results table.
+/// Displays article information and a checkbox for selection.
+#[function_component(Row)]
+pub fn row(props: &RowProps) -> Html {
+    fn doi_link(doi: Option<String>) -> Option<String> {
+        let doi = doi?;
+        Some(format!("https://doi.org/{}", doi))
+    }
+
+    let is_selected = props
+        .article
+        .doi
+        .as_ref()
+        .map(|doi| props.selected_articles.contains(doi))
+        .unwrap_or(false);
+
+    let onchange = {
+        let update_selected = props.update_selected.clone();
+        let doi = props.article.doi.clone();
+        Callback::from(move |event: Event| {
+            let update_selected = update_selected.clone();
+            let doi = doi.clone();
+            let checked = event
+                .target_unchecked_into::<web_sys::HtmlInputElement>()
+                .checked();
+            if let Some(doi) = doi {
+                update_selected.emit((doi, checked))
+            }
+        })
+    };
+
+    html! {
+        <tr>
+            <td><input type={"checkbox"} class={"row-checkbox"} checked={is_selected} onchange={onchange}/></td>
+            <td style=""><a href={doi_link(props.article.doi.clone())} style="word-wrap: break-word">{props.article.doi.clone().unwrap_or_default()}</a></td>
+            <td style="word-wrap: break-word">{props.article.title.clone().unwrap_or_default()}</td>
+            <td style="word-wrap: break-word">{props.article.journal.clone().unwrap_or_default()}</td>
+            <td>{props.article.first_author.clone().unwrap_or_default()}</td>
+            <td>{props.article.year_published.unwrap_or_default()}</td>
+            <td>{props.article.summary.clone().unwrap_or_default()}</td>
+            <td>{props.article.citations.unwrap_or_default()}</td>
+            <td>{props.article.score.unwrap_or_default()}</td>
+        </tr>
+    }
+}
